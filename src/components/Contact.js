8f40302ea44a55b7fffbc34597ee64c7ff5d3ae9
saleheen.noor <saleheen.noor@gmail.com>
2021-03-08 03:32:28 +0500

    minor changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,7 @@ function Contact() {
     return (
         <div className="content">
             LinkedIn: <a href='https://www.linkedin.com/in/saleheen-noor/' target="_blank" >https://www.linkedin.com/in/saleheen-noor/</a>
+            <br />
             Facebook: <a href='https://www.facebook.com/saleheen.noor' target="_blank" >https://www.facebook.com/saleheen.noor</a>
         </div>
     )
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Introduction() {
     return (
         <div className="content">
-            I can develop Web & Mobile Applications using MERN technology such as MongoDB, React Js, React-Native, Express Js, and Node Js.
+            A sophisticated developer & a tech geek who can develop Web & Mobile Applications using MERN technology such as React Js, Redux, React-Native, MongoDB, Express Js, and Node Js.
         </div>
     )
 }
